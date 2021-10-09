98a7c58575b80c99a1835efec8d78631c93ffd12
Xu Yiming <xuyiming.open@outlook.com>
2021-10-09 12:57:47 +0800

    feat: Read header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::Result;
+use std::io::prelude::*;
+
+const SESSION_PATH: &str = "./Session";
+
+fn main() -> Result<()> {
+    let mut file = File::open(SESSION_PATH)?;
+
+    let mut buf: [u8; 4] = [0; 4];
+    file.read_exact(&mut buf)?;
+    assert!(buf == "SNSS".as_bytes());
+
+    println!("{:#04x?}", buf);
+    drop(buf);
+
+    return Ok(());
 }
