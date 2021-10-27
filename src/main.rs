bd9612df3d33d02122dbe0e31177bee55a37151c
Xu Yiming <xuyiming.open@outlook.com>
2021-10-28 02:57:52 +0800

    build: Add byteorder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::Result;
 use std::io::prelude::*;
+use byteorder::{ReadBytesExt, NativeEndian};
 
 const SESSION_PATH: &str = "./Session";
 
@@ -11,8 +12,11 @@ fn main() -> Result<()> {
     file.read_exact(&mut buf)?;
     assert!(buf == "SNSS".as_bytes());
 
+    let version = file.read_i32::<NativeEndian>()?;
+
     println!("{:#04x?}", buf);
     drop(buf);
+    println!("{}", version);
 
     return Ok(());
 }
