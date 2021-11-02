165244ef9bc845fc9f059aaad113f32541cd1beb
Xu Yiming <xuyiming.open@outlook.com>
2021-11-03 00:47:00 +0800

    feat: Add File feature

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::Result;
 use std::io::prelude::*;
+#[macro_use] extern crate lazy_static;
+#[macro_use] extern crate maplit;
 
 const SESSION_PATH: &str = "./Session";
 
@@ -11,6 +13,21 @@ const FILE_SIGNATURE: i32 = 0x53534E53;
 struct FileHeader {
     signature: i32,
     version: i32,
+}
+
+#[derive(Debug)]
+struct FileFeature {
+    encrypted: bool,
+    with_marker: bool,
+}
+
+lazy_static! {
+    static ref FILE_VERSION: std::collections::HashMap<i32, FileFeature> = hashmap! {
+        1 => FileFeature {  encrypted: false,   with_marker: false },
+        2 => FileFeature {  encrypted: true,    with_marker: false },
+        3 => FileFeature {  encrypted: false,   with_marker: true },
+        4 => FileFeature {  encrypted: true,    with_marker: true },
+    };
 }
 
 fn main() -> Result<()> {
@@ -35,6 +52,10 @@ fn main() -> Result<()> {
     assert!(header.signature == FILE_SIGNATURE);
     println!("{:#08x?}", header.signature);
     println!("{}", header.version);
+
+    let capability = FILE_VERSION.get(&header.version).unwrap();
+
+    println!("{:?}", capability);
 
     return Ok(());
 }
