3d1ea07cf02a23258375a2a0b38fd742c15be8a4
Xu Yiming <xuyiming.open@outlook.com>
2022-09-26 21:01:36 +0800

    fix: Serde element unaligned

--- src/serde_chromium_pickle/seq/de.rs
+++ src/serde_chromium_pickle/seq/de.rs
@@ -1,7 +1,127 @@
 use super::super::error::{Error, Result};
-use super::super::Deserializer;
 use serde::de;
 use std::io::prelude::*;
+use std::mem::size_of;
+
+struct ElDeserializer<'a>(&'a mut dyn Read);
+
+impl<'a, 'de> de::Deserializer<'de> for ElDeserializer<'a> {
+    type Error = Error;
+
+    fn is_human_readable(&self) -> bool {
+        return false;
+    }
+
+    fn deserialize_any<V>(self, _visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        unimplemented!();
+    }
+
+    serde::forward_to_deserialize_any! {
+        bool i128 u128 char str string bytes byte_buf option unit
+        unit_struct newtype_struct struct seq tuple tuple_struct map
+        enum identifier
+    }
+
+    fn deserialize_i8<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        let mut bytes: [u8; size_of::<i8>()] = [0; size_of::<i8>()];
+        self.0.read_exact(&mut bytes)?;
+        return visitor.visit_i8(i8::from_ne_bytes(bytes));
+    }
+
+    fn deserialize_i16<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        let mut bytes: [u8; size_of::<i16>()] = [0; size_of::<i16>()];
+        self.0.read_exact(&mut bytes)?;
+        return visitor.visit_i16(i16::from_ne_bytes(bytes));
+    }
+
+    fn deserialize_i32<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        let mut bytes: [u8; size_of::<i32>()] = [0; size_of::<i32>()];
+        self.0.read_exact(&mut bytes)?;
+        return visitor.visit_i32(i32::from_ne_bytes(bytes));
+    }
+
+    fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        let mut bytes: [u8; size_of::<i64>()] = [0; size_of::<i64>()];
+        self.0.read_exact(&mut bytes)?;
+        return visitor.visit_i64(i64::from_ne_bytes(bytes));
+    }
+
+    fn deserialize_u8<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        let mut bytes: [u8; size_of::<u8>()] = [0; size_of::<u8>()];
+        self.0.read_exact(&mut bytes)?;
+        return visitor.visit_u8(u8::from_ne_bytes(bytes));
+    }
+
+    fn deserialize_u16<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        let mut bytes: [u8; size_of::<u16>()] = [0; size_of::<u16>()];
+        self.0.read_exact(&mut bytes)?;
+        return visitor.visit_u16(u16::from_ne_bytes(bytes));
+    }
+
+    fn deserialize_u32<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        let mut bytes: [u8; size_of::<u32>()] = [0; size_of::<u32>()];
+        self.0.read_exact(&mut bytes)?;
+        return visitor.visit_u32(u32::from_ne_bytes(bytes));
+    }
+
+    fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        let mut bytes: [u8; size_of::<u64>()] = [0; size_of::<u64>()];
+        self.0.read_exact(&mut bytes)?;
+        return visitor.visit_u64(u64::from_ne_bytes(bytes));
+    }
+
+    fn deserialize_f32<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        let mut bytes: [u8; size_of::<f32>()] = [0; size_of::<f32>()];
+        self.0.read_exact(&mut bytes)?;
+        return visitor.visit_f32(f32::from_ne_bytes(bytes));
+    }
+
+    fn deserialize_f64<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        let mut bytes: [u8; size_of::<f64>()] = [0; size_of::<f64>()];
+        self.0.read_exact(&mut bytes)?;
+        return visitor.visit_f64(f64::from_ne_bytes(bytes));
+    }
+
+    fn deserialize_ignored_any<V>(self, visitor: V) -> Result<V::Value>
+    where
+        V: de::Visitor<'de>,
+    {
+        return visitor.visit_unit();
+    }
+}
 
 pub(crate) struct SeqDeserializer<'a>(pub(crate) usize, pub(crate) &'a mut dyn Read);
 
@@ -17,7 +137,7 @@ impl<'a, 'de> de::SeqAccess<'de> for SeqDeserializer<'a> {
         } else {
             self.0 -= 1;
 
-            return seed.deserialize(Deserializer(self.1)).map(Into::into);
+            return seed.deserialize(ElDeserializer(self.1)).map(Into::into);
         }
     }
 
--- src/serde_chromium_pickle/seq/ser.rs
+++ src/serde_chromium_pickle/seq/ser.rs
@@ -1,9 +1,128 @@
 use super::super::error::{Error, Result};
-use super::super::Serializer;
 use serde::ser;
 use std::io::prelude::*;
 
 pub(crate) struct SeqSerializer<'a>(pub(crate) &'a mut dyn Write);
+
+type Ok = ();
+
+impl<'a> SeqSerializer<'a> {
+    fn serialize_any_unit(self) -> Result<Ok> {
+        unimplemented!();
+    }
+    fn serialize_any<T: Sized>(self, _v: T) -> Result<Ok> {
+        unimplemented!();
+    }
+    fn serialize_any_ref<T: ?Sized>(self, _v: &T) -> Result<Ok> {
+        unimplemented!();
+    }
+}
+
+impl<'a> ser::Serializer for SeqSerializer<'a> {
+    type Ok = Ok;
+    type Error = Error;
+    type SerializeSeq = ser::Impossible<Self::Ok, Self::Error>;
+    type SerializeTuple = ser::Impossible<Self::Ok, Self::Error>;
+    type SerializeTupleStruct = ser::Impossible<Self::Ok, Self::Error>;
+    type SerializeTupleVariant = ser::Impossible<Self::Ok, Self::Error>;
+    type SerializeMap = ser::Impossible<Self::Ok, Self::Error>;
+    type SerializeStruct = ser::Impossible<Self::Ok, Self::Error>;
+    type SerializeStructVariant = ser::Impossible<Self::Ok, Self::Error>;
+
+    fn is_human_readable(&self) -> bool {
+        return false;
+    }
+
+    crate::forward_to_serialize_any! {
+        bool i128 u128 char str bytes none some unit unit_struct
+        unit_variant newtype_struct newtype_variant // seq tuple
+        // tuple_struct tuple_variant map struct struct_variant
+    }
+
+    fn serialize_i8(self, v: i8) -> Result<Self::Ok> {
+        return Ok(self.0.write_all(&v.to_ne_bytes())?);
+    }
+
+    fn serialize_i16(self, v: i16) -> Result<Self::Ok> {
+        return Ok(self.0.write_all(&v.to_ne_bytes())?);
+    }
+
+    fn serialize_i32(self, v: i32) -> Result<Self::Ok> {
+        return Ok(self.0.write_all(&v.to_ne_bytes())?);
+    }
+
+    fn serialize_i64(self, v: i64) -> Result<Self::Ok> {
+        return Ok(self.0.write_all(&v.to_ne_bytes())?);
+    }
+
+    fn serialize_u8(self, v: u8) -> Result<Self::Ok> {
+        return Ok(self.0.write_all(&v.to_ne_bytes())?);
+    }
+
+    fn serialize_u16(self, v: u16) -> Result<Self::Ok> {
+        return Ok(self.0.write_all(&v.to_ne_bytes())?);
+    }
+
+    fn serialize_u32(self, v: u32) -> Result<Self::Ok> {
+        return Ok(self.0.write_all(&v.to_ne_bytes())?);
+    }
+
+    fn serialize_u64(self, v: u64) -> Result<Self::Ok> {
+        return Ok(self.0.write_all(&v.to_ne_bytes())?);
+    }
+
+    fn serialize_f32(self, v: f32) -> Result<Self::Ok> {
+        return Ok(self.0.write_all(&v.to_ne_bytes())?);
+    }
+
+    fn serialize_f64(self, v: f64) -> Result<Self::Ok> {
+        return Ok(self.0.write_all(&v.to_ne_bytes())?);
+    }
+
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq> {
+        unimplemented!();
+    }
+
+    fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple> {
+        unimplemented!();
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleStruct> {
+        unimplemented!();
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleVariant> {
+        unimplemented!();
+    }
+
+    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap> {
+        unimplemented!();
+    }
+
+    fn serialize_struct(self, _name: &'static str, _len: usize) -> Result<Self::SerializeStruct> {
+        unimplemented!();
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStructVariant> {
+        unimplemented!();
+    }
+}
 
 impl<'a> ser::SerializeSeq for SeqSerializer<'a> {
     type Ok = ();
@@ -13,7 +132,7 @@ impl<'a> ser::SerializeSeq for SeqSerializer<'a> {
     where
         T: ?Sized + ser::Serialize,
     {
-        value.serialize(Serializer(self.0))
+        value.serialize(SeqSerializer(self.0))
     }
 
     fn end(self) -> Result<Self::Ok> {
